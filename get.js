f9320775efe14e4c5482080b2874b3d600fac004
Maria Gabriela <mariagabrielanodejs@gmail.com>
2025-03-23 20:31:55 -0400

    testing...

--- get.js
+++ get.js
@@ -7,13 +7,13 @@ const REDIRECT_URI = 'https://speed-bot-tasas.vercel.app/api/auth/callback/googl
 const oauth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
 
-
+/*
 // Pegar el código obtenido aquí (después de autorizar)
-const CODE = '4/0AQSTgQGknyy6l9hbviG8V0OKoa9xgoUOPl6N-9uPKUQbkAZYLLkLfDHecmQgA1tJBRHGnQ'; // Reemplazar con tu código
+const CODE = '4/0AX4X...'; // Reemplazar con tu código
 
 // Obtener tokens
 oauth2Client.getToken(CODE, (err, tokens) => {
   if (err) return console.error('Error:', err.message);
   console.log('\n2. Configura estos valores en .env.local:');
   console.log('GOOGLE_REFRESH_TOKEN=', tokens.refresh_token);
-});
+});*/
